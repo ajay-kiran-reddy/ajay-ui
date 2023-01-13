49599496fa7f7964619c54f91d9b4375094d7737
ajay-kiran-reddy <ajaykiranreddy999@gmail.com>
2023-01-13 15:32:46 +0530

    vercel added

--- server/app.js
+++ server/app.js
@@ -20,6 +20,11 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 console.log("listening")
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({path: __dirname+'/.env'});
+}
+
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
  app.use(express.static('client/build'));
  app.get('*', (req, res) => {
